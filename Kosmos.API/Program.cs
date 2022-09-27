2d722e8074c96252f0a49d484c68e3c851b01f74
Joakim Westin <yooakim@gmail.com>
2022-09-27 07:27:49 +0200

    WIP: Testing

--- Kosmos.API/Program.cs
+++ Kosmos.API/Program.cs
@@ -1,3 +1,7 @@
+using Kosmos.API.Services;
+using Microsoft.Azure.Cosmos;
+using System.Configuration;
+
 namespace Kosmos.API
 {
     public class Program
@@ -12,6 +16,7 @@ namespace Kosmos.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(builder.Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
 
             var app = builder.Build();
 
@@ -28,6 +33,39 @@ namespace Kosmos.API
             app.MapControllers();
 
             app.Run();
+        }
+
+
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            var databaseName = configurationSection["DatabaseName"];
+            var containerName = configurationSection["ContainerName"];
+            var account = configurationSection["Account"];
+            var key = configurationSection["Key"];
+            var connectionString= configurationSection["ConnectionString"];
+
+            //var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            CosmosClientOptions cosmosClientOptions = new CosmosClientOptions()
+            {
+                HttpClientFactory = () =>
+                {
+                    HttpMessageHandler httpMessageHandler = new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                    };
+
+                    return new HttpClient(httpMessageHandler);
+                },
+                ConnectionMode = ConnectionMode.Gateway                
+            };
+
+            //var client = new CosmosClient(account, key, cosmosClientOptions);
+            var client = new CosmosClient(connectionString, cosmosClientOptions);
+            var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            var cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            return cosmosDbService;
         }
     }
 }
---
+++ Kosmos.API/Services/ICosmosDbService.cs
@@ -0,0 +1,13 @@
+﻿using Kosmos.API.Models;
+
+namespace Kosmos.API.Services
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetMultipleAsync(string query);
+        Task<Item> GetAsync(string id);
+        Task AddAsync(Item item);
+        Task UpdateAsync(string id, Item item);
+        Task DeleteAsync(string id);
+    }
+}
